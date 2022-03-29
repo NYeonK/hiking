e1bf0a7a292506b920df124154f49b29e7ed2b6b
nayeon <skdus728@gmail.com>
2022-03-30 03:11:40 +0900

    MongoDB connection

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express();
 
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://nayeon:qweasdzxc123@cluster0.gzwsj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+  //useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err))
+
 app.listen(8080, function(){
   console.log('listening on 8080')
 });
-
 
 
 app.get('/', function(req, res){
