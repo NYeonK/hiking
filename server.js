a632edfd09994809ac39d317cff5497c8b9467cb
nayeon <skdus728@gmail.com>
2022-04-16 22:31:13 +0900

    Login with Bcrypt

--- server.js
+++ server.js
@@ -62,6 +62,34 @@ app.post('/register', (req, res) => {
     })
 })
 
+app.post('/login', (req, res) => {
+
+  //요청된 email을 DB에서 확인
+  User.findOne({ email: req.body.email }, (err, user) => {
+
+    if(!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "제공된 이메일에 해당하는 유저가 없습니다."
+      })
+    }
+
+    //요청된 email이 DB에 있으면, password가 동일한지 확인
+    user.comparePassword(res.body.password, (err, isMatch) => {
+
+      if(!isMatch) {
+        return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다."})
+      }
+
+      //password가 동일하다면, token 생성
+      user.generateToken((err, user) => {
+        
+      })
+
+    })
+  })
+})
+
 //////////////////////////////////////////////////////////////
 app.use("/community", require("./routes/communityRouter"));
 
