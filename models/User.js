35ddc13fe39bbaa2549e5ba79421b5e1bd0d4428
nayeon <skdus728@gmail.com>
2022-04-16 23:39:14 +0900

    Generate a token with jsonwebtoken

--- models/User.js
+++ models/User.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const saltRounds = 10
+const jwt = require('jsonwebtoken');
+const { JsonWebTokenError } = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
   name: {
@@ -38,7 +40,7 @@ userSchema.pre('save', function( next ){
 
   if(user.isModified('password')){
     
-    // 비밀번호를 암호화 시킨다.
+    // 비밀번호 암호화
     bcrypt.genSalt(saltRounds, function(err, salt) {
       if(err) return next(err)
     
@@ -58,8 +60,24 @@ userSchema.pre('save', function( next ){
 
 userSchema.methods.comparePassword = function(plainPassword, cb) {
   bcrypt.compare(plainPassword, this.password, function(err, isMatch) {
-    if(err) return cb(err),
-      cb(null, isMatch)
+    if(err) return cb(err);
+    cb(null, isMatch);
+  })
+}
+
+userSchema.methods.generateToken = function(cb) {
+
+  var user = this;
+
+  //jsonwebtoken을 이용해서 token 생성
+  var token = jwt.sign(user._id.toHexString(), "secretToken")
+
+  //user._id + 'secretToken' = token
+
+  user.token = token
+  user.save(function(err, user) {
+    if(err) return cb(err);
+    cb(null, user);
   })
 }
 
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 //const cors = require("cors");
 
 const { User } = require("./models/User");
@@ -22,7 +23,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 //application/json
 app.use(bodyParser.json());
-
+app.use(cookieParser());
 
 //mongodb 연결
 const mongoose = require('mongoose')
@@ -48,7 +49,8 @@ app.get('/map', function(req, res){
 
 
 app.post('/register', (req, res) => {
-  // 회원 가입 할 때 필요한 정보들을 client에서 가져오면 DB에 넣어줌.
+
+  //회원 가입 할 때 필요한 정보들을 client에서 가져오면 DB에 넣어줌.
     const user = new User(req.body)
     console.log(req.body)
 
@@ -75,7 +77,7 @@ app.post('/login', (req, res) => {
     }
 
     //요청된 email이 DB에 있으면, password가 동일한지 확인
-    user.comparePassword(res.body.password, (err, isMatch) => {
+    user.comparePassword(req.body.password, (err, isMatch) => {
 
       if(!isMatch) {
         return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다."})
@@ -83,9 +85,12 @@ app.post('/login', (req, res) => {
 
       //password가 동일하다면, token 생성
       user.generateToken((err, user) => {
-        
-      })
+        if (err) return res.status(400).send(err);
 
+        //token 저장
+        res.cookie("x_auth", user.token).status(200).json({ loginSuccess: true, userId: user._id }) 
+
+      })
     })
   })
 })
