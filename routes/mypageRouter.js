019e38b16cfe22f488c12a9c00d94082f0b8bcea
appledaun <appledaun@naver.com>
2022-05-25 15:02:30 +0900

    fix: 회원정보 수정 시 비밀번호 확인

--- routes/mypageRouter.js
+++ routes/mypageRouter.js
@@ -33,23 +33,44 @@ router.post("/main", async (req, res) => {
 });
 
 //회원 정보 수정 - /api/mypage/update
-router.post("/update", async (req, res) => {
-    try {
-        await User.updateOne(
-            { "_id": req.body._id},
-            { $set: {
-                "name": req.body.name,
-                "email": req.body.email,
-                "password" : req.body.password,
-                "image": req.body.image
-            }}
-        );
+router.post("/update", (req, res) => {
+    //요청된 email을 DB에서 확인
+    User.findOne({ _id: req.body._id }, (err, user) => {
+        if(!user) {
+            return res.json({
+                loginSuccess: false,
+                message: "사용자가 존재하지 않습니다."
+            })
+        }
 
-        res.json({ message: true });
-    } catch (err) {
-        console.log(err);
-        res.json({ message: false });
-    }
+        //요청된 _id가 DB에 있으면, password가 동일한지 확인
+        user.comparePassword(req.body.old_password, async (err, isMatch) => {
+
+            if(!isMatch) {
+                console.log("wrong");
+                return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다." })
+            } else {
+                try {
+                    console.log("continue");
+                    await User.updateOne(
+                        { "_id": req.body._id},
+                        { $set: {
+                            "name": req.body.name,
+                            "email": req.body.email,
+                            "password" : req.body.password,
+                            "image": req.body.image
+                        }}
+                    );
+            
+                    res.json({ message: "사용자 정보가 변경되었습니다." });
+                } catch (err) {
+                    console.log(err);
+                    res.json({ message: "사용자 정보 변경에 실패하였습니다." });
+                }
+            }
+        })
+    })
+    
 });
 
 
