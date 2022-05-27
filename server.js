1573f8aaae20e349979c68f09f268986b8fb2831
NYeonK <skdus728@gmail.com>
2022-05-27 23:47:37 +0900

    refactor: 안쓰는 파일 및 코드 정리

--- server.js
+++ server.js
@@ -1,22 +1,7 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-//
-const path = require('path');
-//const favicon = require('static-favicon');
-const session = require('express-session')
-const nodemailer = require('nodemailer');
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const async = require('async');
-const crypto = require('crypto');
-const flash = require('express-flash');
-//
-require('dotenv').config();
 
-//chat-bot
-//const config = require("./")
+require('dotenv').config();
 
 const cors = require("cors");
 
@@ -39,27 +24,7 @@ mongoose.connect(process.env.MONGODB_URI, {
   .catch(err => console.log(err))
 
 
-
-// Middleware
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-//app.use(favicon());
-//app.use(logger('dev'));
-//bodyParser을 통해 아래와 같은 데이터 형식들을 분석해서 가져옴.
-//application/json
-app.use(bodyParser.json());
-//application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(cookieParser());
-app.use(session({ secret: 'session secret key' }));
-app.use(flash());
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(express.static(path.join(__dirname, 'public')));
-
-
-
-
+  
 // Routes
 app.listen(8080, function(){
   console.log('listening on 8080')
